31d8283aba995a6be66c57f4c710cb3ffc35e60e
Tetsuya Mori <monry84@gmail.com>
2017-08-04 18:06:39 +0900

    :new::check_mark_white: Add test code
    * Unity の Play Mode Test を用いる

---
+++ Assets/Tests/Scripts/UnityModule/AnimationEventDispatcherTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityModule;
+
+namespace Tests.UnityModule {
+
+    public class AnimationEventDispatcherTest {
+
+        [UnityTest]
+        [Timeout(10000)]
+        public IEnumerator OnDispatchAsObservableTest() {
+            SceneManager.LoadScene("Tests/Scenes/Test");
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForSeconds(15.0f);
+            GameObject go = GameObject.Find("Test");
+            AnimationEventDispatcher dispatcher = go.GetComponent<AnimationEventDispatcher>();
+            yield return dispatcher
+                .OnDispatchAsObservable("Test")
+                .First()
+                .StartAsCoroutine(
+                    (_) => {
+                        Assert.Pass();
+                    },
+                    (e) => {
+                    }
+                );
+
+        }
+
+    }
+
+}
