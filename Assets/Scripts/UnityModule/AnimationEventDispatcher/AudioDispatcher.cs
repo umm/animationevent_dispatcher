d85e81b00fe9934551340e62a80b624bf4c033be
Tetsuya Mori <monry84@gmail.com>
2017-08-07 11:10:22 +0900

    :recycle: Remove unused `using`
    * エディタ上でスクリプト作ると勝手に追加される using を消してなかった

--- Assets/Scripts/UnityModule/AnimationEventDispatcher/AudioDispatcher.cs
+++ Assets/Scripts/UnityModule/AnimationEventDispatcher/AudioDispatcher.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using GameObjectExtension;
+﻿using GameObjectExtension;
 using UnityEngine;
 
 namespace UnityModule.AnimationEventDispatcher {
