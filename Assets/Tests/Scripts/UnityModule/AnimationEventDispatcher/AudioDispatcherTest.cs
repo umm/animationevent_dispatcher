a4b64be5d6961607d34b5ac0c90643221c88f0f3
Tetsuya Mori <monry84@gmail.com>
2017-08-07 12:23:53 +0900

    :white_check_mark: Implement Test for AudioDispatcher
    * UniRx を使った AudioDispatcher のテストを書いた
    * 5秒経過しても実行されない場合は result が true にならないので Assert 転ける

--- Assets/Tests/Scripts/UnityModule/AnimationEventDispatcher/AudioDispatcherTest.cs
+++ Assets/Tests/Scripts/UnityModule/AnimationEventDispatcher/AudioDispatcherTest.cs
@@ -15,21 +15,21 @@ namespace Tests.UnityModule {
             SceneManager.LoadScene("Tests/Scenes/Test");
             yield return new WaitForEndOfFrame();
             yield return new WaitForEndOfFrame();
-//            GameObject go = GameObject.Find("Audio");
-//            AudioDispatcher dispatcher = go.GetComponent<AudioDispatcher>();
-//            bool result = false;
-//            yield return Observable.EveryUpdate().Delay(System.TimeSpan.FromMilliseconds(600)).StartAsCoroutine()
-//                .OnDispatchAsObservable("Test")
-//                .Timeout(System.TimeSpan.FromSeconds(5))
-//                .First()
-//                .StartAsCoroutine(
-//                    (_) => {
-//                        result = true;
-//                    },
-//                    (e) => {
-//                    }
-//                );
-//            Assert.IsTrue(result, "Dispatch されたかどうか");
+            GameObject go = GameObject.Find("Audio");
+            AudioDispatcher dispatcher = go.GetComponent<AudioDispatcher>();
+            bool result = false;
+            yield return dispatcher
+                .OnDispatchAsObservable()
+                .Timeout(System.TimeSpan.FromSeconds(5))
+                .First()
+                .StartAsCoroutine(
+                    (_) => {
+                        result = true;
+                    },
+                    (e) => {
+                    }
+                );
+            Assert.IsTrue(result, "Dispatch されたかどうか");
         }
 
     }
