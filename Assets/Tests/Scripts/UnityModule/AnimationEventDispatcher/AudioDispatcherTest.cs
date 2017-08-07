feb4c42034db2c81e9a3279a0434826f64fbb8ff
Tetsuya Mori <monry84@gmail.com>
2017-08-07 12:01:51 +0900

    :new: Add Assets for TestCase for AudioDispatcher
    * AudioDispatcher のテスト用のアセットを追加

---
+++ Assets/Tests/Scripts/UnityModule/AnimationEventDispatcher/AudioDispatcherTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityModule.AnimationEventDispatcher;
+
+namespace Tests.UnityModule {
+
+    public class AudioDispatcherTest {
+
+        [UnityTest]
+        public IEnumerator OnDispatchAsObservableTest() {
+            SceneManager.LoadScene("Tests/Scenes/Test");
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+//            GameObject go = GameObject.Find("Audio");
+//            AudioDispatcher dispatcher = go.GetComponent<AudioDispatcher>();
+//            bool result = false;
+//            yield return Observable.EveryUpdate().Delay(System.TimeSpan.FromMilliseconds(600)).StartAsCoroutine()
+//                .OnDispatchAsObservable("Test")
+//                .Timeout(System.TimeSpan.FromSeconds(5))
+//                .First()
+//                .StartAsCoroutine(
+//                    (_) => {
+//                        result = true;
+//                    },
+//                    (e) => {
+//                    }
+//                );
+//            Assert.IsTrue(result, "Dispatch されたかどうか");
+        }
+
+    }
+
+}
