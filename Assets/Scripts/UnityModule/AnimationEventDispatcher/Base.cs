ac2d3070973024335605f77f8ff5955f7b1ecf4f
Tetsuya Mori <monry84@gmail.com>
2017-08-07 12:15:20 +0900

    :sparkles::truck: Move stream field to Base class
    * GeneralDispatcher からも AudioDispatcher からも参照したいので

---
+++ Assets/Scripts/UnityModule/AnimationEventDispatcher/Base.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+namespace UnityModule.AnimationEventDispatcher {
+
+    public abstract class Base : MonoBehaviour {
+
+        /// <summary>
+        /// AnimationEvent Dispatch 時に発火するストリームの実体
+        /// </summary>
+        private Subject<AnimationEvent> streamAnimationEvent;
+
+        /// <summary>
+        /// AnimationEvent Dispatch 時に発火するストリーム
+        /// </summary>
+        /// <remarks>外から触られたくなかったので protected にしています</remarks>
+        protected Subject<AnimationEvent> StreamAnimationEvent {
+            get {
+                if (this.streamAnimationEvent == default(Subject<AnimationEvent>)) {
+                    this.streamAnimationEvent = new Subject<AnimationEvent>();
+                }
+                return this.streamAnimationEvent;
+            }
+            set {
+                this.streamAnimationEvent = value;
+            }
+        }
+
+    }
+
+}
