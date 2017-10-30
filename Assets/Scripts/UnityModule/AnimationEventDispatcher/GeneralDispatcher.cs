20e9bcdc9669a18c9285a41ec6b9c5d96f19e44e
Tetsuya Mori <monry84@gmail.com>
2017-10-30 21:53:38 +0900

    :sparkles: Implement General AnimationEvent
    * BeginAnimation, EndAnimation を共通的にメソッドとして切り出す

--- Assets/Scripts/UnityModule/AnimationEventDispatcher/GeneralDispatcher.cs
+++ Assets/Scripts/UnityModule/AnimationEventDispatcher/GeneralDispatcher.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UniRx;
+using UnityEngine;
 
 namespace UnityModule.AnimationEventDispatcher {
 
@@ -8,11 +9,51 @@ namespace UnityModule.AnimationEventDispatcher {
     public class GeneralDispatcher : Base {
 
         /// <summary>
+        /// 汎用イベント名: アニメーション開始
+        /// </summary>
+        private const string ANIMATION_EVENT_NAME_BEGIN_ANIMATION = "BeginAnimation";
+
+        /// <summary>
+        /// 汎用イベント名: アニメーション終了
+        /// </summary>
+        private const string ANIMATION_EVENT_NAME_END_ANIMATION = "EndAnimation";
+
+        /// <summary>
         /// AnimationEvent を Dispatch する
         /// </summary>
         /// <param name="animationEvent">Inspector で設定する情報を含んだ AnimationEvent</param>
         public void Dispatch(AnimationEvent animationEvent) {
             this.StreamAnimationEvent.OnNext(animationEvent);
+        }
+
+        /// <summary>
+        /// アニメーション開始を表す AnimationEvent を Dispatch する
+        /// </summary>
+        public void DispatchBeginAnimation() {
+            this.StreamAnimationEvent.OnNext(new AnimationEvent() { stringParameter = ANIMATION_EVENT_NAME_BEGIN_ANIMATION});
+        }
+
+        /// <summary>
+        /// アニメーション終了を表す AnimationEvent を Dispatch する
+        /// </summary>
+        public void DispatchEndAnimation() {
+            this.StreamAnimationEvent.OnNext(new AnimationEvent() { stringParameter = ANIMATION_EVENT_NAME_END_ANIMATION});
+        }
+
+        /// <summary>
+        /// Dispatch されたアニメーション開始を表す AnimationEvent を UniRx ストリームとして返す
+        /// </summary>
+        /// <returns>AnimationEvent のストリーム</returns>
+        public IObservable<AnimationEvent> OnDispatchBeginAnimation() {
+            return this.OnDispatchAsObservable(ANIMATION_EVENT_NAME_BEGIN_ANIMATION);
+        }
+
+        /// <summary>
+        /// Dispatch されたアニメーション終了を表す AnimationEvent を UniRx ストリームとして返す
+        /// </summary>
+        /// <returns>AnimationEvent のストリーム</returns>
+        public IObservable<AnimationEvent> OnDispatchEndAnimation() {
+            return this.OnDispatchAsObservable(ANIMATION_EVENT_NAME_BEGIN_ANIMATION);
         }
 
     }
